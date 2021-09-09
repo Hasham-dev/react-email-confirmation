9e65fafe85066aae7e415e25879f86562fe2417f
Hasham-dev <vakanihasham64@gmail.com>
2021-09-10 01:53:50 +0500

    update readme & fixes

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://mighty-coast-85233.herokuapp.com/'
+  ? 'https://mighty-coast-85233.herokuapp.com'
   : 'http://localhost:8080'
